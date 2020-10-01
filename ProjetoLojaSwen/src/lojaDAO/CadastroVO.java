2cbd505b0f74dc068e0d7d58a2a5e85a15ce398b
Thaís <thais_de_souza@hotmail.com>
2020-09-30 21:14:35 -0300

    Criação de gets e sets

--- ProjetoLojaSwen/src/lojaDAO/CadastroVO.java
+++ ProjetoLojaSwen/src/lojaDAO/CadastroVO.java
@@ -1,5 +1,57 @@
 package lojaDAO;
 
 public class CadastroVO {
-
+	
+	public String Nome;
+	public int CPF;
+	public int Telefone;
+	public String Email;
+	public double Salario;
+	public String Função;
+	public String Endereço;
+	
+	
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public int getCPF() {
+		return CPF;
+	}
+	public void setCPF(int cPF) {
+		CPF = cPF;
+	}
+	public int getTelefone() {
+		return Telefone;
+	}
+	public void setTelefone(int telefone) {
+		Telefone = telefone;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public double getSalario() {
+		return Salario;
+	}
+	public void setSalario(double salario) {
+		Salario = salario;
+	}
+	public String getFunção() {
+		return Função;
+	}
+	public void setFunção(String função) {
+		Função = função;
+	}
+	public String getEndereço() {
+		return Endereço;
+	}
+	public void setEndereço(String endereço) {
+		Endereço = endereço;
+	}
+	
 }
