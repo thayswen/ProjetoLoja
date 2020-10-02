266e37d27e056e872faea6afece18f3d37cadfcf
Thaís <thais_de_souza@hotmail.com>
2020-10-02 19:05:22 -0300

    Foi feita a inclusão das outras propriedades na classe

--- ProjetoLojaSwen/src/lojaPrincipal/servico/ClienteServico.java
+++ ProjetoLojaSwen/src/lojaPrincipal/servico/ClienteServico.java
@@ -12,13 +12,31 @@ public class ClienteServico {
 	private ClienteDAO dao = new ClienteDAO();
 	private Cadastro cadastro;
 	private String nome;
+	private int cpf;
+	private int telefone;
+	private String email;
 	Scanner input = new Scanner(System.in);
 
 	public void incluirCliente() {
 		cadastro = new Cadastro();
+		
 		System.out.println("Insira o nome do cadastro: ");
 		nome = input.nextLine();
 		cadastro.setNome(nome);
+		
+		System.out.println("Insira o seu CPF: ");
+		cpf = input.nextInt();
+		cadastro.setCPF(cpf);
+		
+		System.out.println("Insira o seu telefone: ");
+		telefone = input.nextInt();
+		cadastro.setTelefone(telefone);
+		
+		System.out.println("Insira o seu email do cadastro: ");
+		email = input.nextLine();
+		cadastro.setEmail(email);
+		
+		
 		dao.incluirCadastro(cadastro);
 
 	}
